9665be8570c6fce815a3e236589e0accdf120220
Josef <josef.grieb@gmail.com>
2018-05-07 19:18:45 +0200

    implement feature and tests

--- java/src/main/java/berlin/code/tddchallenge/TaxCalculator.java
+++ java/src/main/java/berlin/code/tddchallenge/TaxCalculator.java
@@ -1,27 +1,77 @@
 package berlin.code.tddchallenge;
 
+import berlin.code.tddchallenge.data.Product;
 import berlin.code.tddchallenge.data.ShoppingCart;
+import berlin.code.tddchallenge.data.ShoppingCartItem;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+import java.util.Map;
 
 public class TaxCalculator {
+    private Map<String,Product> products;
+    private List<ShoppingCartItem> shoppingCartItems;
 
     public TaxCalculator(Database db, ShoppingCart cart) {
-        // TODO
+        products = db.getProducts();
+        shoppingCartItems = cart.getContents();
     }
 
-    public double getGrandtotal(double taxRate) {
-        return 0; // TODO
+    public double getGrandtotalTaxAmount() {
+        int grandTotal = 0;
+        for(ShoppingCartItem shoppingCartItem : shoppingCartItems) {
+            grandTotal += getTaxAmount(shoppingCartItem.getProductId(),shoppingCartItem.getQuantity());
+        }
+        return grandTotal;
     }
 
-    public double getGrandtotalTax(double taxRate) {
-        return 0; // TODO
+    public double getGrandtotalTax() {
+        int grandTotalTax = 0;
+        for(ShoppingCartItem shoppingCartItem : shoppingCartItems) {
+            grandTotalTax += products.get(shoppingCartItem.getProductId()).getPrice();
+        }
+        return grandTotalTax;
+    }
+
+    public int getSubTotalCount(double taxRate) {
+        return 0;
+    }
+
+
+
+    public int getGrantTotalCount() {
+        int grandTotalCount = 0;
+        for(ShoppingCartItem shoppingCartItem : shoppingCartItems) {
+            grandTotalCount += shoppingCartItem.getQuantity();
+        }
+        return grandTotalCount;
     }
 
     public double getSubtotal(double taxRate) {
-        return 0; // TODO
+        return 0; //
     }
 
     public double getSubtotalTax(double taxRate) {
-        return 0; // TODO
+        return 0;
     }
+
+    public double getPriceTotal(String productId,int quantity) {
+        return products.get(productId).getPrice() * quantity;
+    }
+
+    public double getTaxAmount(String productId, int quantity) {
+        Product product = products.get(productId);
+        double productTaxRate = product.getTaxRate();
+        double netPrice = getNetPrice(productTaxRate,product.getPrice());
+        double rawTax = product.getPrice() - netPrice;
+        return rawTax * quantity;
+    }
+
+    private double getNetPrice(double productTaxRate,double productPrice) {
+        return  productPrice / (1 + productTaxRate);
+    }
+
+
 
 }
--- java/src/test/java/berlin/code/tddchallenge/test/TaxCalculatorTest.java
+++ java/src/test/java/berlin/code/tddchallenge/test/TaxCalculatorTest.java
@@ -2,6 +2,7 @@ package berlin.code.tddchallenge.test;
 
 import berlin.code.tddchallenge.Database;
 import berlin.code.tddchallenge.TaxCalculator;
+import berlin.code.tddchallenge.data.Product;
 import berlin.code.tddchallenge.data.ShoppingCart;
 import berlin.code.tddchallenge.data.ShoppingCartItem;
 import org.junit.jupiter.api.*;
@@ -33,12 +34,71 @@ public class TaxCalculatorTest {
         }
     }
 
+
     @Test
     public void calculationTest() {
         TaxCalculator calc = new TaxCalculator(db, cart);
 
-        // TODO write integration test
+        assertEquals(2.60,calc.getSubtotal(0),0.005,"unexpected subtotal for 0%");
+        assertEquals(9.98,calc.getSubtotal(0.07),0.005,"unexpected subtotal for 7%");
+        assertEquals(20.97,calc.getSubtotal(0.19),0.005,"unexpected subtotal for 19%");
 
-        assertEquals(2.89, calc.getSubtotalTax(.07), 0.005, "unexpected subtotal for 7%");
+        assertEquals(3.35,calc.getSubtotalTax(0.07),0.005,"unexpected subtotalTax for 0%");
+        assertEquals(0.65, calc.getSubtotalTax(0.07), 0.005, "unexpected subtotalTax for 7%");
+        assertEquals(3.35,calc.getSubtotalTax(0.19),0.005,"unexpected subtotalTax for 19%");
+
+        assertEquals(12.30,calc.getGrandtotalTax(),0.005,"unexpected getGrandtotalTax");
+        assertEquals(4.00,calc.getGrandtotalTaxAmount(),0.005,"unexpected getGrandtotalTaxAmount");
+    }
+
+
+    @Test
+    public void subTotalCounterTest() {
+        TaxCalculator taxCalculator = new TaxCalculator(db,cart);
+        double subTotalCounterZero = taxCalculator.getSubTotalCount(0);
+        double subTotalCounterSeven = taxCalculator.getSubTotalCount(0.07);
+        double subTotalCounterNineTeen = taxCalculator.getSubTotalCount(0.19);
+
+        assertEquals(4,subTotalCounterZero);
+        assertEquals(11,subTotalCounterSeven);
+        assertEquals(3,subTotalCounterNineTeen);
+
+    }
+
+    @Test
+    public void subTotalGrandTest() {
+        TaxCalculator taxCalculator = new TaxCalculator(db,cart);
+
+        double grandTotalCount = taxCalculator.getGrantTotalCount();
+        assertEquals(18,grandTotalCount);
+    }
+
+
+    @Test
+    public void priceTotalTest() {
+        TaxCalculator taxCalculator = new TaxCalculator(db,cart);
+
+        double appleTaxAmount = taxCalculator.getTaxAmount("apple",5);
+        double orangeTaxAmount = taxCalculator.getTaxAmount("orange",4);
+        double milkTaxAmount = taxCalculator.getTaxAmount("milk",2);
+        double redWineTaxAmount = taxCalculator.getTaxAmount("red wine",3);
+        double stampsTaxAmount = taxCalculator.getTaxAmount("stamps",4);
+
+        assertEquals(0.26,appleTaxAmount,0.005, "unexptected getTaxAmountTotal apple");
+        assertEquals(0.24,orangeTaxAmount,0.005, "unexptected getTaxAmountTotal orange");
+        assertEquals(0.16,milkTaxAmount,0.005, "unexptected getTaxAmountTotal milk");
+        assertEquals(3.35,redWineTaxAmount,0.005, "unexptected getTaxAmountTotal red wine");
+        assertEquals(0.00,stampsTaxAmount,0.005, "unexptected getTaxAmountTotal stamps");
+    }
+
+    @Test
+    public void priceEachTest() {
+        TaxCalculator taxCalculator = new TaxCalculator(db,cart);
+
+        double appleGetPriceTotal = taxCalculator.getPriceTotal("apple",5);
+        double orangeTaxAmountPriceTotal = taxCalculator.getPriceTotal("orange",4);
+
+        assertEquals(4.00,appleGetPriceTotal);
+        assertEquals(4.00,orangeTaxAmountPriceTotal);
     }
 }
